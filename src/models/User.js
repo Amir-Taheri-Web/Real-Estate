905f9dac21bb55fea1f9c49c59dadc66bbe8654f
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-30 15:49:29 +0330

    feat: User schema and model created

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+});
+
+const User = models.User || model("User", userSchema);
+
+export default User;

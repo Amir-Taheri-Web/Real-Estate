7290fab6ed28bc330e1359e288eb80aa7567410d
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-30 19:21:35 +0330

    feat: signin and signout api created

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,56 @@
+import User from "@/models/User";
+import { verifyPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import { e2p } from "@/utils/convert";
+import { validateEmail, validatePassword } from "@/utils/validate";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("مشکلی در اتصال به دیتابیس پیش آمده");
+        }
+
+        const { email, password } = credentials;
+
+        if (!email.trim() || !password.trim())
+          throw new Error("لطفا تمام فیلدها را پر کنید");
+
+        if (!validateEmail(email)) throw new Error("ایمیل معتبر نیست");
+
+        if (!validatePassword(password))
+          throw new Error(
+            e2p(
+              "رمز عبور باید لاتین، حداقل 8 کاراکتر با حروف کوچک و بزرگ، عدد و یک حرف خاص باشد"
+            )
+          );
+
+        try {
+          const user = await User.findOne({ email });
+
+          if (!user) throw new Error("کاربری با این مشخضات وجود ندارد");
+
+          const isPasswordValid = await verifyPassword(password, user.password);
+
+          if (!isPasswordValid) throw new Error("ایمیل یا رمز عبور اشتباه است");
+
+          return { email };
+        } catch (error) {
+          console.log(error);
+
+          throw new Error("مشکلی در ارتباط با سرور به وجود آمده");
+        }
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };

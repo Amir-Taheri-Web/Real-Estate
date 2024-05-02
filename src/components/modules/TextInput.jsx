52dad952a5747f818a8101d8106da65e194b774e
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-02 10:32:06 +0330

    feat: add-profile page created

--- src/components/modules/TextInput.jsx
+++ src/components/modules/TextInput.jsx
@@ -1,22 +1,19 @@
 import { e2p } from "@/utils/convert";
 
-const TextInput = ({
-  placeholder,
-  name,
-  value,
-  textArea = false,
-  changeHandler,
-}) => {
+const TextInput = ({ placeholder, name, value, changeHandler }) => {
   return (
     <div>
       <label htmlFor={name}>{placeholder}:</label>
-      {textArea ? (
+
+      {name === "description" && (
         <textArea
           value={e2p(value[name])}
           name={name}
           onChange={changeHandler}
         ></textArea>
-      ) : (
+      )}
+
+      {name !== "description" && (
         <input
           type="text"
           value={e2p(value[name])}

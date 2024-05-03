a6a0a0070f22559cc03ce3c19f7f3585965cbd23
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-03 11:47:07 +0330

    feat: dashboard card added

--- src/components/modules/DashboardCard.jsx
+++ src/components/modules/DashboardCard.jsx
@@ -1,9 +1,20 @@
 import ProfileCard from "./ProfileCard";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 
 const DashboardCard = ({ profile }) => {
   return (
     <div>
       <ProfileCard profile={profile} />
+      <div>
+        <button type="button">
+          <FaEdit />
+        </button>
+
+        <button type="button">
+          <MdDeleteForever />
+        </button>
+      </div>
     </div>
   );
 };

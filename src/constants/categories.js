e1cf6a212077291f6c83413b9e4bc2e1d510cbc7
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-03 11:19:51 +0330

    chore: category related icons added

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,3 +1,8 @@
+import { FaHouseChimney } from "react-icons/fa6";
+import { MdApartment } from "react-icons/md";
+import { IoStorefront } from "react-icons/io5";
+import { PiOfficeChairFill } from "react-icons/pi";
+
 const categories = {
   villa: "ویلا",
   apartment: "آپارتمان",
@@ -5,6 +10,11 @@ const categories = {
   office: "دفتر",
 };
 
-const categoriesIcons = {};
+const categoriesIcons = {
+  villa: <FaHouseChimney />,
+  apartment: <MdApartment />,
+  store: <IoStorefront />,
+  office: <PiOfficeChairFill />,
+};
 
 export { categories, categoriesIcons };

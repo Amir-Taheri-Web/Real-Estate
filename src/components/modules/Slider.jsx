58ccd13448b3adb4deb0c688f53663891af31246
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-04 10:43:13 +0330

    style: main page style added

--- src/components/modules/Slider.jsx
+++ src/components/modules/Slider.jsx
@@ -4,16 +4,28 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+const Arrow = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "none" }}
+      onClick={onClick}
+    />
+  );
+};
+
 const SliderComponent = ({ children }) => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     speed: 3000,
     autoplaySpeed: 3000,
     cssEase: "linear",
+    nextArrow: <Arrow />,
+    prevArrow: <Arrow />,
   };
   return (
     <div className="slider-container">
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,15 +1,52 @@
+import { categories } from "@/constants/categories";
 import { cities } from "@/constants/cities";
+import Profile from "@/models/Profile";
+import ProfileCard from "@/modules/ProfileCard";
 import SliderComponent from "@/modules/Slider";
+import connectDB from "@/utils/connectDB";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/HomePage.module.css";
 
-const HomePage = () => {
+const HomePage = async () => {
+  await connectDB();
+
+  const profiles = await Profile.find().select("-userId");
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.cities}>
         <h2>شهرهای پر طرفدار</h2>
         <SliderComponent>
           {cities.map((city, index) => (
             <div key={index}>
               <span>{city}</span>
+            </div>
+          ))}
+        </SliderComponent>
+      </div>
+
+      <ul className={styles.categories}>
+        {Object.keys(categories).map((category, index) => (
+          <li key={index}>
+            <Link href="">
+              <Image
+                src={`/images/${category}.jpg`}
+                width={1920}
+                height={1080}
+                alt={`${category} image`}
+              />
+              <span>{categories[category]}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles.profiles}>
+        <SliderComponent>
+          {profiles.map((profile) => (
+            <div key={profile._id}>
+              <ProfileCard profile={profile} />
             </div>
           ))}
         </SliderComponent>

0666224a940a0e3b1e2d6e3c0331aec58355ca0b
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-05 15:10:01 +0330

    fix: sitemap updated

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,12 +1,24 @@
+import Profile from "@/models/Profile";
+import connectDB from "@/utils/connectDB";
+
 const sitemap = async () => {
   const staticRoutes = ["", "/profiles"];
+
+  await connectDB();
+  const res = await Profile.find().select("-userId");
+  const profiles = await res.json();
 
   const routes1 = staticRoutes.map((route) => ({
     url: `http://localhost:3000/${route}`,
     lastModified: new Date().toString(),
   }));
 
-  return [...routes1];
+  const routes2 = profiles.map((profile) => ({
+    url: `http://localhost:3000/profiles/${profile._id}`,
+    lastModified: new Date().toString(),
+  }));
+
+  return [...routes1, ...routes2];
 };
 
 export default sitemap;

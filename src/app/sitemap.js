4615c15d969bf79f329339a170f5b01ceb49c2a5
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-06 10:35:45 +0330

    fix: sitemap updated

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,24 +1,12 @@
-import Profile from "@/models/Profile";
-import connectDB from "@/utils/connectDB";
-
 const sitemap = async () => {
   const staticRoutes = ["", "/profiles"];
-
-  await connectDB();
-  const res = await Profile.find().select("-userId");
-  const profiles = await res.json();
 
   const routes1 = staticRoutes.map((route) => ({
     url: `${process.env.URL}/${route}`,
     lastModified: new Date().toString(),
   }));
 
-  const routes2 = profiles.map((profile) => ({
-    url: `${process.env.URL}/profiles/${profile._id}`,
-    lastModified: new Date().toString(),
-  }));
-
-  return [...routes1, ...routes2];
+  return [...routes1];
 };
 
 export default sitemap;

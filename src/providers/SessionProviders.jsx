ae30e3563eeede4b2d0bca90c3fe1f462e31bc49
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-06 10:52:46 +0330

    fix: sessionProvider updated

--- src/providers/SessionProviders.jsx
+++ src/providers/SessionProviders.jsx
@@ -3,7 +3,7 @@
 import { SessionProvider } from "next-auth/react";
 
 const SessionProviders = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 };
 
 export default SessionProviders;
